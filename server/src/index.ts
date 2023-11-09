5b6d6a96ac198bf243554e8f8ea80935a7c2ad98
류일한 <ryuilhan@lyuilhans-MacBook-Air.local>
2023-11-09 22:23:57 +0900

    add: image upload

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,13 @@ import dotenv from 'dotenv';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import multer from 'multer';
 import { ChatStreamService } from '@/chat/chat.stream.service';
 import { ChatStaticService } from '@/chat/chat.static.service';
 import { VisionService } from '@/vision/vision.service';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 const app = express();
 
@@ -31,6 +35,35 @@ app.post('/chat/static', function (req, res) {
 
 app.post('/vision', function (req, res) {
 	VisionService(req, res);
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+	  cb(null, path.join(__dirname, '../temp')); // 파일이 저장될 경로
+	},
+	filename: (req, file, cb) => {
+	  const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+	  cb(null, uniqueSuffix + path.extname(file.originalname)); // 파일 이름 설정
+	}
+  });
+  
+  const upload = multer({ storage: storage });
+  
+
+app.post('/food', upload.single('image'), function (req, res) {
+	console.log('DEBUG: food');
+	if (req.file) {
+		res.json({
+		  success: true,
+		  message: 'File uploaded successfully!',
+		  fileInfo: req.file
+		});
+	  } else {
+		res.status(400).json({ success: false, message: 'No file uploaded.' });
+	  }
 });
 
 server.listen(port, () => {
