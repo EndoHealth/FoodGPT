a90cafbfb5d605f1a567c9d20672a559f213bae2
huisang-growth <huisang.growth@gmail.com>
2023-11-10 23:59:51 +0900

    feat: get rid of unnecessary files

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,14 +4,11 @@ import http from 'http';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import multer from 'multer';
-import { ChatStreamService } from '@/chat/chat.stream.service';
-import { ChatStaticService } from '@/chat/chat.static.service';
-import { VisionService } from '@/vision/vision.service';
-import { MailService } from '@/mail/mail.service';
+import { VisionService } from './modules/vision/vision.service';
+import { MailService } from './modules/mail/mail.service';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { FoodService } from '@/food/food.service';
-
+import { FoodService } from './modules/food/food.service';
 
 const app = express();
 
@@ -27,14 +24,6 @@ app.get('/check', function (req, res) {
 	res.send('alive');
 });
 
-app.post('/chat/stream', function (req, res) {
-	ChatStreamService(req, res);
-});
-
-app.post('/chat/static', function (req, res) {
-	ChatStaticService(req, res);
-});
-
 app.post('/vision', function (req, res) {
 	VisionService(req, res);
 });
@@ -43,7 +32,11 @@ app.post('/mail', async function (req, res) {
 	const { email, body } = req.body;
 	console.log(email);
 	const mailService = new MailService();
-	await mailService.sendEmail(email, `Today's Meal Nutrition Breakdown and Insights`, body);
+	await mailService.sendEmail(
+		email,
+		`Today's Meal Nutrition Breakdown and Insights`,
+		body
+	);
 	res.send('success');
 });
 
