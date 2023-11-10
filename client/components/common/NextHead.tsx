6b1949f590fd2ca62a4c23e7dbe59cb0e11e7919
huisang-growth <huisang.growth@gmail.com>
2023-11-10 23:17:47 +0900

    fix: add OG tags, change production url

--- client/components/common/NextHead.tsx
+++ client/components/common/NextHead.tsx
@@ -5,18 +5,18 @@ export const NextHead = () => {
 	return (
 		<Head>
 			<meta charSet="utf-8" />
-			<title>title</title>
-			<link rel="canonical" href="superchat.im" />
+			<title>FoodGPT</title>
+			<link rel="canonical" href="food-gpt-vision.vercel.app" />
 			<link rel="shortcut icon" href="/favicon.ico" />
-			<meta property="og:title" content="title" />
-			<meta property="og:description" content="description" />
-			<meta property="og:url" content="superchat.im" />
+			<meta property="og:title" content="FoodGPT" />
+			<meta property="og:description" content="Your automated food diary" />
+			<meta property="og:url" content="food-gpt-vision.vercel.app" />
 			<meta property="og:type" content="website" />
-			<meta property="og:locale" content="ko_KR" />
-			<meta property="og:image" content={'/.png'} />
+			<meta property="og:locale" content="en_US" />
+			<meta property="og:image" content={'/camera.png'} />
 			<meta property="twitter:cardType" content="summary_large_image" />
-			<meta property="twitter:handle" content="@handle" />
-			<meta property="twitter:site" content="@site" />
+			<meta property="twitter:handle" content="@heesang-md" />
+			<meta property="twitter:site" content="@heesang-md" />
 		</Head>
 	);
 };
--- client/environment.ts
+++ client/environment.ts
@@ -1,4 +1,5 @@
-export const API_URL = 'http://localhost:8080';
+// export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://api.huisangyun.com';
 export const GA_ID = 'G-D8GEH8DM6Q';
 export const HOTJAR_ID = '3729615';
 export const TWITTER_URL = 'https://twitter.com/heesang_md';
