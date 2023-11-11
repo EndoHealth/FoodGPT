d3459b6484edeabc147442fb3d06ce64c34212eb
huisang-growth <huisang.growth@gmail.com>
2023-11-11 13:41:34 +0900

    feat: initial commit

--- client/recoil/atoms.ts
+++ client/recoil/atoms.ts
@@ -1,15 +1,4 @@
-import { User } from 'common/models';
 import { atom } from 'recoil';
-
-export const userState = atom<User>({
-	key: 'userState',
-	default: {} as User,
-});
-
-export const isBottomsheetVisibleState = atom<boolean>({
-	key: 'isBottomsheetVisibleState',
-	default: false,
-});
 
 export const resultState = atom<string>({
 	key: 'resultState',
